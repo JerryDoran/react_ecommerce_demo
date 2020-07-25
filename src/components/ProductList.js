b83f61a7f9b47aec26c489056e55c80f15d23fda
Jerome Doran <disneyfan9184@gmail.com>
2020-07-25 17:53:52 -0400

    Added Product Context Provider and Consumer

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import Product from './Product';
+import { Title } from './Title';
+import { storeProducts } from '../data';
+import { ProductContextConsumer } from '../context/ProductContext';
+import { createGlobalStyle } from 'styled-components';
 
 export default class ProductList extends Component {
   render() {
     return (
-      <div>
-        <Product />
-      </div>
+      <>
+        <div className='py-5'>
+          <div className='container'>
+            {/* Parent component passing down two props to the Title (child) component */}
+            <Title name='our' title='products' />
+            <div className='row'>
+              <ProductContextConsumer>
+                {(value) => {
+                  console.log(value);
+                }}
+              </ProductContextConsumer>
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import ProductContextProvider from './context/ProductContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ProductContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </ProductContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
