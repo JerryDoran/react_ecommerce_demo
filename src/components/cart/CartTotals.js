d97412b228b04296a75f4f95b6492259c4402bba
Jerome Doran <disneyfan9184@gmail.com>
2020-09-06 16:59:55 -0400

    Added paypal

--- src/components/cart/CartTotals.js
+++ src/components/cart/CartTotals.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PaypalButton from './PaypalButton';
 
-const CartTotals = ({ value }) => {
+const CartTotals = ({ value, history }) => {
   const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
   return (
     <>
@@ -18,17 +19,22 @@ const CartTotals = ({ value }) => {
               </button>
             </Link>
             <h5>
-              <span className="text-title">subtotal : </span>
+              <span className='text-title'>subtotal : </span>
               <strong>${cartSubTotal}</strong>
             </h5>
             <h5>
-              <span className="text-title">tax : </span>
+              <span className='text-title'>tax : </span>
               <strong>${cartTax}</strong>
             </h5>
             <h5>
-              <span className="text-title">total : </span>
+              <span className='text-title'>total : </span>
               <strong>${cartTotal}</strong>
             </h5>
+            <PaypalButton
+              total={cartTotal}
+              clearCart={clearCart}
+              history={history}
+            />
           </div>
         </div>
       </div>
